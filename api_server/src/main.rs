8c515d720fa57a037131b4e1cff5c89a7e236071
Freddie Brown <fred@noser.net>
2020-06-15 16:11:28 +0100

    add CORS information to request headers

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -1,9 +1,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 extern crate serde_json;
-extern crate rocket_contrib;
 
 #[cfg(test)] mod tests;
 
@@ -13,6 +13,10 @@ use rocket::Request;
 // use rocket::response::Redirect;
 use rocket_contrib::templates::Template;
 use rocket_contrib::serve::StaticFiles;
+use rocket::http::Method;
+use rocket::{get, routes};
+use rocket_contrib::json::{Json, JsonValue};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Error, CorsOptions};
 
 #[derive(Serialize)]
 struct TemplateContext {
@@ -21,10 +25,14 @@ struct TemplateContext {
 }
 
 #[get("/")]
-fn index() -> Template {
-    let name = "Person".to_string();
-    let context = TemplateContext { name, items: vec!["One", "Two", "Three"] };
-    Template::render("index", &context)
+fn index() -> JsonValue {
+    // let name = "Person".to_string();
+    // let context = TemplateContext { name, items: vec!["One", "Two", "Three"] };
+    // Template::render("index", &context)
+    json!({
+        "name": "Freddie",
+        "age": 22,
+    })
 }
 
 #[get("/hello")]
@@ -39,14 +47,21 @@ fn not_found(req: &Request) -> Template {
     Template::render("error/404", &map)
 }
 
-fn rocket() -> rocket::Rocket {
+fn main() -> Result<(), Error> {
+    let cors = CorsOptions {
+        allowed_origins: AllowedOrigins::some_exact(&["http://localhost:3000"]),
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }.to_cors()?;
+
     rocket::ignite()
         .mount("/api", routes![index, hello])
         .mount("/api", StaticFiles::from("static/"))
         .attach(Template::fairing())
-        .register(catchers![not_found])
-}
+        .attach(cors)
+        .register(catchers![not_found]).launch();
 
-fn main() {
-    rocket().launch();
+    Ok(())
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+
+  state = {
+    contacts: [],
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3001/api")
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({contacts: data})
+    })
+    .catch(console.log)
+  }
+
+  public render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src="logo.png" className="App-logo" alt="logo" />
         <h1>
           Welcome to Sybl
         </h1>
         <h3>Distributed ML with Ensemble Methods</h3>
+        <p>
+          
+        </p>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
