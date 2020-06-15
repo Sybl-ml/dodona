da646b12588073df4ddf6f042a7ad601b7d749ec
Freddie Brown <fred@noser.net>
2020-06-15 14:18:09 +0100

    changes base website and adds logo
    Adds in images needed to show logo and alters the CSS and HTML to show this

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.png" className="App-logo" alt="logo" />
+        <h1>
+          Welcome to Sybl
+        </h1>
+        <h3>Distributed ML with Ensemble Methods</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
