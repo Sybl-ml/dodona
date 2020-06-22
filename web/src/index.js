78da86f6ba89e2a1cec7121f4a08386c5b583046
Nathan Hall <hallnath42@gmail.com>
2020-06-22 04:08:51 +0100

    Custom font using Google Fonts (test font)

--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import WebFont from 'webfontloader';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+WebFont.load({
+  google: {
+    families: ['Krona One']
+  }
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
