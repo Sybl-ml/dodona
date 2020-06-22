71b8c04627b152716bd89a0bfdc01e5944eb96bf
Nathan Hall <nathanhall@Nathans-MacBook-Pro.local>
2020-06-22 03:53:24 +0100

    Added sample text to landing page

--- web/src/App.js
+++ web/src/App.js
@@ -65,8 +65,12 @@ class App extends Component {
             {console.log(contacts)}
         </ul>
       </header>
-    </div>
-    );
+  <h1>Heading 1</h1>
+  <h2>Heading 2</h2>
+  <h3>Heading 3</h3>
+	    <p> Testing sentence of testingly test </p>
+  </div>
+   );
   }
 }
 
