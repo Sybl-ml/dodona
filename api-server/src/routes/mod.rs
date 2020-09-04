240146f5de89d0e0d4738be9df206176eb5b255c
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-09-04 12:05:56 +0100

    Rename unused Request argument

--- api-server/src/routes/mod.rs
+++ api-server/src/routes/mod.rs
@@ -3,12 +3,12 @@ use tide::{http::mime, Request, Response};
 
 pub mod users;
 
-pub async fn index(req: Request<State>) -> tide::Result<impl Into<Response>> {
+pub async fn index(_req: Request<State>) -> tide::Result<impl Into<Response>> {
     Ok(Response::builder(200)
         .body(json!({"name": "Freddie", "age": 22}))
         .content_type(mime::JSON))
 }
 
-pub async fn hello(req: Request<State>) -> tide::Result<impl Into<Response>> {
+pub async fn hello(_req: Request<State>) -> tide::Result<impl Into<Response>> {
     Ok("Hey from Dodona!")
 }
