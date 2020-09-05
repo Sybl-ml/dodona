aaf70d39a48cf11919eb49723e240731ff87a15b
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2020-09-05 12:43:17 +0100

    Add negative hash verification tests

--- api-server/src/core/tests.rs
+++ api-server/src/core/tests.rs
@@ -17,10 +17,22 @@ fn test_hash_strings() {
 }
 
 #[test]
-fn test_verify() {
+fn test_verify_positive() {
     let salt = auth::generate_chars(64);
     let password = auth::generate_chars(128);
     let hash = auth::hash(&password, &salt);
-    let verify = auth::verify(&password, &salt, hash);
-    assert!(verify);
+    assert!(auth::verify(&password, &salt, hash));
+}
+
+#[test]
+fn test_verify_negative() {
+    let salt = auth::generate_chars(64);
+    let password_a = auth::generate_chars(128);
+    let password_b = auth::generate_chars(128);
+    let hash_a = auth::hash(&password_a, &salt);
+    let hash_b = auth::hash(&password_b, &salt);
+    if password_a != password_b {
+        assert!(!auth::verify(&password_a, &salt, hash_b));
+        assert!(!auth::verify(&password_b, &salt, hash_a));
+    }
 }
