7d63655d75cd4a08891758275d67c6bcd2e1a034
Nathan Hall <hallnath42@gmail.com>
2020-09-18 01:18:37 +0100

    More development on dashboard

--- web/src/components/DashNavbar.js
+++ web/src/components/DashNavbar.js
@@ -3,6 +3,7 @@ import { Navbar, Nav,} from 'react-bootstrap';
 import styled from "styled-components";
 import MemoLogo from '../icons/Logo.js';
 import {PrimaryButton} from './Buttons';
+import { FaAngleDown } from 'react-icons/fa';
 
 import Toggle from "./Toggler"
 
@@ -10,9 +11,18 @@ const HeaderBar = styled(Navbar)`
     background: ${({ theme }) => theme.body};
     transition: all 0.25s linear;
 `;
+
+const UserIcon = styled.img`
+	background-color: #bbb;
+	border-radius: 50%;
+	height:2.5rem;
+	width:2.5rem;
+	margin-left: 0.5rem;
+`;
+
 const DashHeader = ({theme, toggleTheme}) => {
         return (
-		<HeaderBar collapseOnSelect expand="lg" sticky="top">
+		<HeaderBar collapseOnSelect sticky="top">
 			<HeaderBar.Brand href="/home">
 				<MemoLogo 
 					theme={theme} 
@@ -21,10 +31,12 @@ const DashHeader = ({theme, toggleTheme}) => {
 			<HeaderBar.Toggle aria-controls="responsive-navbar-nav" />
   			<HeaderBar.Collapse id="responsive-navbar-nav" className="justify-content-end">
 				<Toggle theme={theme} toggleTheme={toggleTheme} />
-				<Nav>
-					<Nav.Link href="/login">Sign In</Nav.Link>
+				<Nav className="justify-content-center">
+					<UserIcon src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" />
+					<Nav.Link style={{paddingLeft:"0", paddingRight:"0"}}>
+						<FaAngleDown/>
+					</Nav.Link>
 				</Nav>
-				<PrimaryButton variant="primary" href="#">Hey, John</PrimaryButton>
 			</HeaderBar.Collapse>
   		</HeaderBar>
         );
--- web/src/components/Dashboard.js
+++ web/src/components/Dashboard.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
+import { Container, Row, Col, Tabs, Tab, InputGroup, FormControl } from 'react-bootstrap';
 import styled from "styled-components";
 import DashHeader from "./DashNavbar";
-import { ProjectCard, ModelCard } from "./Cards"
+import { ProjectCard, ModelCard } from "./Cards";
+import { FaSearch } from 'react-icons/fa';
 
 const MaxCol = styled(Col)`
     height:100vh;
 `;
 
+const SideBar = styled(Tabs)`
+    
+`;
+
+const Top = styled.div`
+    height:6rem;
+`;
 const Dashboard = ({theme, toggleTheme}) => {
         return (
             <>
@@ -15,8 +23,15 @@ const Dashboard = ({theme, toggleTheme}) => {
             <DashHeader theme={theme} toggleTheme={toggleTheme}/>
             <Container fluid>
                 <Row>
-                    <MaxCol xs={{span:12, order: 'last'}} lg={{span:4, order: 'first'}} xl={3}>
-                        <Tabs defaultActiveKey="home" transition={false} defaultActiveKey="projects">
+                    <MaxCol xs={{span:12, order: 'last'}} lg={{span:3, order: 'first'}}>
+                        <Top>
+                            <InputGroup className="mb-2" style={{padding:"1.5rem 0"}}>
+                                <FormControl id="inlineFormInputGroup" placeholder="Search">
+                                    
+                                </FormControl>
+                           </InputGroup>
+                        </Top>
+                        <SideBar defaultActiveKey="home" transition={false} defaultActiveKey="projects">
                             <Tab eventKey="projects" title="Projects">
                                 <ProjectCard 
                                     title="Project #6"
@@ -46,11 +61,25 @@ const Dashboard = ({theme, toggleTheme}) => {
                                     title="This one is bad haha"
                                 />
                             </Tab>
+                        </SideBar>
+                    </MaxCol>
+                    <Col xs={12} lg={9} style={{textAlign:"left"}}>
+                        <Top>
+                            <h2>Project #3</h2>
+                            <h5>Here is a desciption of some stuff...</h5>
+                        </Top>
+                        <Tabs defaultActiveKey="overview" transition={false}>
+                            <Tab eventKey="overview" title="Overview">
+
+                            </Tab>
+                            <Tab eventKey="input" title="Input Data">
+
+                            </Tab>
+                            <Tab eventKey="output" title="Output Results">
+
+                            </Tab>
                         </Tabs>
-                    </MaxCol>
-                    <MaxCol xs={12} lg={8} xl={9} style={{backgroundColor: "cyan"}}>
-                        2
-                    </MaxCol>
+                    </Col>
                 </Row>
             </Container>
             </>
