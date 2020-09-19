d6c9947c88f62c2faae5a9a3d00995095c3f309a
Freddie Brown <fred@noser.net>
2020-09-19 16:18:33 +0100

    added pepper to login process

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -14,6 +14,7 @@ async fn main() -> Result<(), std::io::Error> {
     dotenv().ok();
     let conn_str = env::var("CONN_STR").expect("CONN_STR must be set");
     let app_name = env::var("APP_NAME").expect("APP_NAME must be set");
+    let pepper = env::var("PEPPER").expect("PEPPER must be set");
 
     // Configuring DB connection
     let mut client_options = match ClientOptions::parse(&conn_str).await {
@@ -31,6 +32,7 @@ async fn main() -> Result<(), std::io::Error> {
     let engine = State {
         client: Arc::new(client),
         db_name: Arc::new(String::from("sybl")),
+        pepper: Arc::new(pepper),
     };
 
     let mut app = tide::with_state(engine);
