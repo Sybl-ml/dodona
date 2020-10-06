c6303f57b1647447d8528743942dba71315e3d49
Graham <grahamk206@gmail.com>
2020-10-06 20:43:59 +0100

    add route /u/user_id and get project

--- api-server/src/routes/projects.rs
+++ api-server/src/routes/projects.rs
@@ -5,7 +5,41 @@ use tide::{Request, Response};
 
 use crate::models::model::Model;
 use crate::models::projects::Project;
+use crate::models::users::User;
 use crate::State;
+
+pub async fn get_all(req: Request<State>) -> tide::Result {
+    let state = req.state();
+    let db = state.client.database("sybl");
+
+    let mut cursor = Project::find(db.clone(), doc! {}, None).await?;
+    let mut docs: Vec<Project> = Vec::new();
+    while let Some(proj) = cursor.next().await {
+        docs.push(proj?);
+    }
+    Ok(Response::builder(200)
+        .body(json!(docs))
+        .content_type(mime::JSON)
+        .build())
+}
+
+pub async fn get_project(req: Request<State>) -> tide::Result {
+    let state = req.state();
+    let db = state.client.database("sybl");
+
+    let project_id: String = req.param("project_id")?;
+    let object_id = ObjectId::with_string(&project_id).unwrap();
+
+    let filter = doc! { "_id": object_id };
+
+    let doc = Project::find_one(db.clone(), filter, None).await?;
+    let response = Response::builder(200)
+        .body(json!(doc))
+        .content_type(mime::JSON)
+        .build();
+
+    Ok(response)
+}
 
 pub async fn get_user_projects(req: Request<State>) -> tide::Result {
     let state = req.state();
@@ -14,27 +48,18 @@ pub async fn get_user_projects(req: Request<State>) -> tide::Result {
     let user_id: String = req.param("user_id")?;
     let object_id = ObjectId::with_string(&user_id).unwrap();
 
-    let filter = doc! { "user_id": object_id };
+    // TODO check that user exists in users table when requesting projects
+    let filter = doc! { "user_id": &object_id };
+
+    match User::find_one(db.clone(), doc! { "_id": &object_id}, None).await? {
+        None => return Ok(Response::builder(404).body("user not found").build()),
+        _ => (),
+    };
+
     let mut cursor = Project::find(db.clone(), filter, None).await?;
     let mut docs: Vec<Project> = Vec::new();
-    while let Some(user) = cursor.next().await {
-        docs.push(user?);
-    }
-    Ok(Response::builder(200)
-        .body(json!(docs))
-        .content_type(mime::JSON)
-        .build())
-}
-
-pub async fn get_all(req: Request<State>) -> tide::Result {
-    let state = req.state();
-    let db = state.client.database("sybl");
-
-    let mut cursor = Project::find(db.clone(), doc! {}, None).await?;
-    let mut docs: Vec<Project> = Vec::new();
-    while let Some(user) = cursor.next().await {
-        log::info!("{:?}", &user);
-        docs.push(user?);
+    while let Some(proj) = cursor.next().await {
+        docs.push(proj?);
     }
     Ok(Response::builder(200)
         .body(json!(docs))
