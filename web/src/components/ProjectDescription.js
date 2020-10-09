209a5384e681cb6370683286fcfa5e4d9f703f2c
Graham <grahamk206@gmail.com>
2020-10-09 09:54:13 +0100

    make project nested route

--- web/src/components/ProjectDescription.js
+++ web/src/components/ProjectDescription.js
@@ -1,33 +1,27 @@
 import React from "react";
-import {
-    Container,
-    Row,
-    Col,
-    Tabs,
-    Tab,
-    InputGroup,
-    FormControl,
-} from "react-bootstrap";
+import { Tabs, Tab } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Top = styled.div`
   height: 6rem;
 `;
 
-const ProjectDescription = ({ theme, toggleTheme, match }) => {
-    console.log(match.params.projectid)
-    return (
-        <>
-            <Top>
-                <h2>Project #3</h2>
-                <h5>Here is a desciption of some stuff...</h5>
-            </Top>
-            <Tabs defaultActiveKey="overview" transition={false}>
-                <Tab eventKey="overview" title="Overview"></Tab>
-                <Tab eventKey="input" title="Input Data"></Tab>
-                <Tab eventKey="output" title="Output Results"></Tab>
-            </Tabs>
-        </>
-    )
-}
-export default ProjectDescription
+const ProjectDescription = ({ theme, toggleTheme }) => {
+  let { projectid } = useParams();
+  console.log(projectid);
+  return (
+    <>
+      <Top>
+        <h2>Project #{projectid}</h2>
+        <h5>Here is a desciption of some stuff...</h5>
+      </Top>
+      <Tabs defaultActiveKey="overview" transition={false}>
+        <Tab eventKey="overview" title="Overview"></Tab>
+        <Tab eventKey="input" title="Input Data"></Tab>
+        <Tab eventKey="output" title="Output Results"></Tab>
+      </Tabs>
+    </>
+  );
+};
+export default ProjectDescription;
