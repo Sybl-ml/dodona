3a5b998d7f8401a5bc8bef692c54eb1c9be9886a
Graham <grahamk206@gmail.com>
2020-10-11 17:22:59 +0100

    GET routes for projects

--- api-server/src/routes/projects.rs
+++ api-server/src/routes/projects.rs
@@ -1,75 +1,61 @@
 use async_std::stream::StreamExt;
-use mongodb::bson::{doc, oid::ObjectId};
+use mongodb::bson::{doc, document::Document, oid::ObjectId};
 use tide::http::mime;
 use tide::{Request, Response};
 
-use crate::models::model::Model;
 use crate::models::projects::Project;
 use crate::models::users::User;
+use crate::routes::response_from_json;
 use crate::State;
 
 pub async fn get_all(req: Request<State>) -> tide::Result {
-    let state = req.state();
-    let db = state.client.database("sybl");
+    let database = req.state().client.database("sybl");
+    let projects = database.collection("projects");
 
-    let mut cursor = Project::find(db.clone(), doc! {}, None).await?;
-    let mut docs: Vec<Project> = Vec::new();
-    while let Some(proj) = cursor.next().await {
-        docs.push(proj?);
-    }
-    Ok(Response::builder(200)
-        .body(json!(docs))
-        .content_type(mime::JSON)
-        .build())
+    let cursor = projects.find(None, None).await?;
+    let documents: Result<Vec<Document>, mongodb::error::Error> = cursor.collect().await;
+
+    Ok(response_from_json(documents.unwrap()))
 }
 
 pub async fn get_project(req: Request<State>) -> tide::Result {
-    let state = req.state();
-    let db = state.client.database("sybl");
+    let database = req.state().client.database("sybl");
+    let projects = database.collection("projects");
 
     let project_id: String = req.param("project_id")?;
     let object_id = match ObjectId::with_string(&project_id) {
         Ok(id) => id,
-        Err(_e) => return Ok(Response::builder(404).body("user not found").build()),
+        Err(_e) => return Ok(Response::builder(404).body("invalid project id").build()),
     };
 
     let filter = doc! { "_id": object_id };
 
-    let doc = Project::find_one(db.clone(), filter, None).await?;
-    let response = Response::builder(200)
-        .body(json!(doc))
-        .content_type(mime::JSON)
-        .build();
-
-    Ok(response)
+    let doc = projects.find_one(filter, None).await?.unwrap();
+    let proj: Project = mongodb::bson::de::from_document(doc).unwrap();
+    Ok(response_from_json(proj))
 }
 
 pub async fn get_user_projects(req: Request<State>) -> tide::Result {
-    let state = req.state();
-    let db = state.client.database("sybl");
+    let database = req.state().client.database("sybl");
+    let projects = database.collection("projects");
+    let users = database.collection("users");
 
     let user_id: String = req.param("user_id")?;
     let object_id = match ObjectId::with_string(&user_id) {
         Ok(id) => id,
-        Err(_e) => return Ok(Response::builder(404).body("user not found").build()),
+        Err(_e) => return Ok(Response::builder(404).body("invalid user id").build()),
     };
 
-    let filter = doc! { "user_id": &object_id };
-
-    if User::find_one(db.clone(), doc! { "_id": &object_id}, None)
+    if users
+        .find_one(doc! { "_id": &object_id}, None)
         .await?
         .is_none()
     {
         return Ok(Response::builder(404).body("user not found").build());
     }
+    let filter = doc! { "user_id": &object_id };
+    let cursor = projects.find(filter, None).await?;
+    let documents: Result<Vec<Document>, mongodb::error::Error> = cursor.collect().await;
 
-    let mut cursor = Project::find(db.clone(), filter, None).await?;
-    let mut docs: Vec<Project> = Vec::new();
-    while let Some(proj) = cursor.next().await {
-        docs.push(proj?);
-    }
-    Ok(Response::builder(200)
-        .body(json!(docs))
-        .content_type(mime::JSON)
-        .build())
+    Ok(response_from_json(documents.unwrap()))
 }
