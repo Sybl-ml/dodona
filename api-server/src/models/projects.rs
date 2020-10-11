7a8e9e2ac3328c822585eb7d0ea5a05f26e44864
Graham <grahamk206@gmail.com>
2020-10-11 17:22:35 +0100

    add projects model

--- api-server/src/models/projects.rs
+++ api-server/src/models/projects.rs
@@ -2,8 +2,6 @@ use mongodb::bson;
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
-use crate::models::model;
-
 // Define a model. Simple as deriving a few traits.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Project {
@@ -12,16 +10,4 @@ pub struct Project {
     pub name: String,
     pub date_created: bson::DateTime,
     pub user_id: Option<ObjectId>,
-}
-
-impl model::Model for Project {
-    const COLLECTION_NAME: &'static str = "projects";
-
-    fn id(&self) -> Option<ObjectId> {
-        self.id.clone()
-    }
-
-    fn set_id(&mut self, id: ObjectId) {
-        self.id = Some(id);
-    }
 }
