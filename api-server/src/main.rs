2e8c2f43c6a012acf5b1079e397e841f5cd415c8
Freddie Brown <fred@noser.net>
2020-10-12 11:54:08 +0100

    added new endpoint

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -11,6 +11,36 @@ async fn main() -> Result<(), std::io::Error> {
     dodona::load_config(environment);
     let app = dodona::build_server().await;
 
+<<<<<<< HEAD
+=======
+    let mut user_api = core_api.at("/users");
+    user_api.at("/:user_id").get(routes::users::get);
+    user_api.at("/filter").post(routes::users::filter);
+    user_api.at("/edit").post(routes::users::edit);
+    user_api.at("/login").post(routes::users::login);
+    user_api.at("/new").post(routes::users::new);
+    user_api.at("/delete").post(routes::users::delete);
+
+    let mut projects_api = core_api.at("/projects");
+    projects_api
+        .at("/u/:user_id")
+        .get(routes::projects::get_user_projects);
+    projects_api.at("/").get(routes::projects::get_all);
+    projects_api
+        .at("/p/:project_id")
+        .get(routes::projects::get_project);
+    projects_api.at("/new").post(routes::projects::new);
+
+    // CORS
+    let cors = CorsMiddleware::new()
+        .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+        .allow_origin(Origin::from("*"))
+        .allow_credentials(false);
+
+    app.with(cors);
+
+    // Serving App
+>>>>>>> added new endpoint
     tide::log::start();
     app.listen("0.0.0.0:3001").await?;
 
