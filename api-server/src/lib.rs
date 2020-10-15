29aa1b806212d0558066d5b42d7f7a956aed42e5
Alexander Jackson <alexanderjackson@protonmail.com>
2020-10-15 21:18:51 +0100

    Add unit tests for /api/users (#42)
    * Add some basic tests
    
    Add some basic tests under the `tests/` directory. There are only two
    tests, which setup the server and ping two endpoints, one of which
    requires a connection to the MongoDB server itself.
    
    * Add unit tests for /api/users
    
    Add more unit tests for /api/users routes. This includes failure
    conditions, such as registering with the same email as a previous user
    and logging in with the incorrect password.
    
    This should be used in conjunction with a local database instance for
    testing purposes. It will delete all stored data upon running `cargo
    test`, before adding some test data. This can be done on the Atlas
    instance, but may lead to some issues if people run tests at the same
    time.
    
    In the future, it may be easier to mock the MongoDB instance to mitigate
    the need for this, however for now it should be fine to setup the
    database and run some API queries on the data.
    
    * Move dotenv::dotenv() back to main
    
    Move the environment populator back to `src/main.rs` as the
    `std::once::Once` in `tests/api.rs` will populate it anyway and it
    doesn't make sense for it to be here.
    
    * Add some basic tests
    
    Add some basic tests under the `tests/` directory. There are only two
    tests, which setup the server and ping two endpoints, one of which
    requires a connection to the MongoDB server itself.
    
    * Add unit tests for /api/users
    
    Add more unit tests for /api/users routes. This includes failure
    conditions, such as registering with the same email as a previous user
    and logging in with the incorrect password.
    
    This should be used in conjunction with a local database instance for
    testing purposes. It will delete all stored data upon running `cargo
    test`, before adding some test data. This can be done on the Atlas
    instance, but may lead to some issues if people run tests at the same
    time.
    
    In the future, it may be easier to mock the MongoDB instance to mitigate
    the need for this, however for now it should be fine to setup the
    database and run some API queries on the data.
    
    * Move dotenv::dotenv() back to main
    
    Move the environment populator back to `src/main.rs` as the
    `std::once::Once` in `tests/api.rs` will populate it anyway and it
    doesn't make sense for it to be here.
    
    * Return id.to_string() instead of id
    
    Return the stringified version of id as it causes less nesting in the
    resulting JSON.

--- api-server/src/lib.rs
+++ api-server/src/lib.rs
@@ -1,9 +1,13 @@
 #[macro_use]
 extern crate serde_json;
 
+use std::env;
 use std::sync::Arc;
 
+use http_types::headers::HeaderValue;
+use mongodb::options::ClientOptions;
 use mongodb::Client;
+use tide::security::{CorsMiddleware, Origin};
 
 pub mod models;
 pub mod routes;
@@ -13,4 +17,58 @@ pub struct State {
     pub client: Arc<Client>,
     pub db_name: Arc<String>,
     pub pepper: Arc<String>,
+}
+
+pub async fn build_server() -> tide::Server<State> {
+    let conn_str = env::var("CONN_STR").expect("CONN_STR must be set");
+    let app_name = env::var("APP_NAME").expect("APP_NAME must be set");
+    let pepper = env::var("PEPPER").expect("PEPPER must be set");
+
+    // Configuring DB connection
+    let mut client_options = ClientOptions::parse(&conn_str).await.unwrap();
+    client_options.app_name = Some(app_name);
+
+    let client = Client::with_options(client_options).unwrap();
+
+    let engine = State {
+        client: Arc::new(client),
+        db_name: Arc::new(String::from("sybl")),
+        pepper: Arc::new(pepper),
+    };
+
+    let mut app = tide::with_state(engine);
+
+    // Setting up routes
+    let mut core_api = app.at("/api");
+    core_api.at("/").get(routes::index);
+
+    let mut user_api = core_api.at("/users");
+    user_api.at("/:user_id").get(routes::users::get);
+    user_api.at("/filter").post(routes::users::filter);
+    user_api.at("/edit").post(routes::users::edit);
+    user_api.at("/login").post(routes::users::login);
+    user_api.at("/new").post(routes::users::new);
+    user_api.at("/delete").post(routes::users::delete);
+
+    let mut projects_api = core_api.at("/projects");
+    projects_api
+        .at("/u/:user_id")
+        .get(routes::projects::get_user_projects);
+    projects_api.at("/").get(routes::projects::get_all);
+    projects_api
+        .at("/p/:project_id")
+        .get(routes::projects::get_project);
+
+    // CORS
+    let cors = CorsMiddleware::new()
+        .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+        .allow_origin(Origin::from("*"))
+        .allow_credentials(false);
+
+    app.with(cors);
+
+    // Serving App
+    app.at("/").get(|_| async { Ok("Hello, world!") });
+
+    app
 }
