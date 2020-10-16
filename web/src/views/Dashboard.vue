967593aa6b330b3d225dedd10ecdcc6be1be0607
Graham Kemp <44637230+G-Kemp101@users.noreply.github.com>
2020-10-16 15:07:11 +0100

    Vue refactor (#46)
    * replace react with vue
    
    * ignore node_modules
    
    * change to web folder
    
    * Remove yarn run
    
    * Fixed the primary colour (#41)
    
    * removed web2
    
    * api calls in dashboard
    
    * use npm not yarn
    
    * ignore .env file
    
    * ignore .env file
    
    * replace object_id with user_id to collect users projects
    
    * ignore rust target
    
    * Delete README.md
    
    This is old can add new readme for frontend but isnt really required. You literally just use docker
    
    * Add keras and tensorflow logo
    
    * text centering
    
    * GET and list all projects for user
    
    * Add nested project view route
    
    Co-authored-by: Nathan Hall <37386274+hallnath1@users.noreply.github.com>

---
+++ web/src/views/Dashboard.vue
@@ -0,0 +1,77 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col xs="12" order-xs="2" lg="3">
+        <b-row>
+          <b-col class="mb-3">
+            <b-form-input placeholder="Search" block />
+          </b-col>
+        </b-row>
+        <b-row class="text-left">
+          <b-col>
+            <router-link :to="{ name: 'AddProject' }">
+              <b-button variant="primary" class="mb-1 add-new" block
+                ><b-row
+                  ><b-col class="text-left">Add new project</b-col
+                  ><b-col class="ml-auto text-right">
+                    <b-icon-plus-circle /></b-col></b-row></b-button
+            ></router-link>
+            <router-link
+              v-for="p in projects"
+              :key="p.id"
+              :to="{
+                name: `ProjectView`,
+                params: {
+                  projectId: p.id,
+                  name: p.name,
+                  description: p.description,
+                },
+              }"
+            >
+              <b-card :title="p.name" class="mb-1">
+                <b-card-text>
+                  {{ p.description }}
+                </b-card-text>
+              </b-card>
+            </router-link>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col>
+        <router-view></router-view>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<style>
+.add-new {
+  height: 70px;
+  font-size: large;
+}
+</style>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  async mounted() {
+    let user_id = $cookies.get("token");
+
+    let response = await axios.get(
+      `http://localhost:3001/api/projects/u/${user_id}`
+    );
+
+    this.projects = response.data.map((x) => {
+      let y = { id: x._id.$oid, ...x };
+      delete y._id;
+      return y;
+    });
+  },
+  methods: {},
+};
+</script>
