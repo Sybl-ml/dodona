3975c0fb1010054d8f083cffc10369e8020aca76
Alexander Jackson <alexanderjackson@protonmail.com>
2020-10-17 14:43:54 +0100

    Add pbkdf2_iterations to the config
    Add the `pbkdf2_iterations` variable to the configuration file, allowing
    people to use different iteration counts depending on the environment.
    This allows less iterations for tests for example, so they can be
    completed quicker.
    
    Remove the checking of hashes when adding a new user, as this is really
    just a sanity check and wasn't doing anything with the result.

--- api-server/tests/config.rs
+++ api-server/tests/config.rs
@@ -1,4 +1,6 @@
-use dodona::config::{ConfigFile, Environment};
+use std::str::FromStr;
+
+use dodona::config::{Config, ConfigFile, Environment};
 
 static TEST_CONFIG: &str = r#"
 [global]
@@ -14,33 +16,49 @@ pepper = "develop"
 
 [testing]
 conn_str = "localhost"
+pbkdf2_iterations = 1000
 "#;
 
 #[test]
 fn resolve_production_config() {
     let config = ConfigFile::from_str(TEST_CONFIG).resolve(Environment::Production);
 
-    assert_eq!(config.app_name, Some("Sybl".into()));
-    assert_eq!(config.conn_str, None);
-    assert_eq!(config.pepper, Some("prod".into()));
+    let expected = Config {
+        app_name: Some("Sybl".into()),
+        conn_str: None,
+        pepper: Some("prod".into()),
+        pbkdf2_iterations: None,
+    };
+
+    assert_eq!(config, expected);
 }
 
 #[test]
 fn resolve_development_config() {
     let config = ConfigFile::from_str(TEST_CONFIG).resolve(Environment::Development);
 
-    assert_eq!(config.app_name, Some("Dodona".into()));
-    assert_eq!(config.conn_str, None);
-    assert_eq!(config.pepper, Some("develop".into()));
+    let expected = Config {
+        app_name: Some("Dodona".into()),
+        conn_str: None,
+        pepper: Some("develop".into()),
+        pbkdf2_iterations: None,
+    };
+
+    assert_eq!(config, expected);
 }
 
 #[test]
 fn resolve_testing_config() {
     let config = ConfigFile::from_str(TEST_CONFIG).resolve(Environment::Testing);
 
-    assert_eq!(config.app_name, Some("Dodona".into()));
-    assert_eq!(config.conn_str, Some("localhost".into()));
-    assert_eq!(config.pepper, Some("default".into()));
+    let expected = Config {
+        app_name: Some("Dodona".into()),
+        conn_str: Some("localhost".into()),
+        pepper: Some("default".into()),
+        pbkdf2_iterations: Some(1000),
+    };
+
+    assert_eq!(config, expected);
 }
 
 #[test]
@@ -48,7 +66,19 @@ fn environment_variables_are_updated() {
     let config = ConfigFile::from_str(TEST_CONFIG).resolve(Environment::Testing);
     config.populate_environment();
 
-    assert_eq!(std::env::var("APP_NAME"), Ok("Dodona".into()));
-    assert_eq!(std::env::var("CONN_STR"), Ok("localhost".into()));
-    assert_eq!(std::env::var("PEPPER"), Ok("default".into()));
+    let app_name = std::env::var("APP_NAME").ok();
+    let conn_str = std::env::var("CONN_STR").ok();
+    let pepper = std::env::var("PEPPER").ok();
+    let pbkdf2_iterations = std::env::var("PBKDF2_ITERATIONS")
+        .ok()
+        .map(|x| u32::from_str(&x).unwrap());
+
+    let environment = Config {
+        app_name,
+        conn_str,
+        pepper,
+        pbkdf2_iterations,
+    };
+
+    assert_eq!(environment, config);
 }
