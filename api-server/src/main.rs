d466f6ab6ea5f3cbc9fca2bae6b31135b2087c2b
Alexander Jackson <alexanderjackson@protonmail.com>
2020-10-20 14:07:37 +0100

    Revert "Add unit tests for /api/users (#42)"
    This reverts commit 29aa1b806212d0558066d5b42d7f7a956aed42e5.

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -1,10 +1,68 @@
+use std::env;
+use std::sync::Arc;
+
+use dotenv::dotenv;
+use http_types::headers::HeaderValue;
+use mongodb::options::ClientOptions;
+use mongodb::Client;
+use tide::security::{CorsMiddleware, Origin};
+
+use dodona::routes;
+use dodona::State;
+
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
-    dotenv::dotenv().unwrap();
-    let app = dodona::build_server().await;
+    dotenv().ok();
+    let conn_str = env::var("CONN_STR").expect("CONN_STR must be set");
+    let app_name = env::var("APP_NAME").expect("APP_NAME must be set");
+    let pepper = env::var("PEPPER").expect("PEPPER must be set");
 
+    // Configuring DB connection
+    let mut client_options = ClientOptions::parse(&conn_str).await.unwrap();
+    client_options.app_name = Some(app_name);
+
+    let client = Client::with_options(client_options).unwrap();
+
+    let engine = State {
+        client: Arc::new(client),
+        db_name: Arc::new(String::from("sybl")),
+        pepper: Arc::new(pepper),
+    };
+
+    let mut app = tide::with_state(engine);
+
+    // Setting up routes
+    let mut core_api = app.at("/api");
+    core_api.at("/").get(routes::index);
+
+    let mut user_api = core_api.at("/users");
+    user_api.at("/:user_id").get(routes::users::get);
+    user_api.at("/filter").post(routes::users::filter);
+    user_api.at("/edit").post(routes::users::edit);
+    user_api.at("/login").post(routes::users::login);
+    user_api.at("/new").post(routes::users::new);
+    user_api.at("/delete").post(routes::users::delete);
+
+    let mut projects_api = core_api.at("/projects");
+    projects_api
+        .at("/u/:user_id")
+        .get(routes::projects::get_user_projects);
+    projects_api.at("/").get(routes::projects::get_all);
+    projects_api
+        .at("/p/:project_id")
+        .get(routes::projects::get_project);
+
+    // CORS
+    let cors = CorsMiddleware::new()
+        .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+        .allow_origin(Origin::from("*"))
+        .allow_credentials(false);
+
+    app.with(cors);
+
+    // Serving App
     tide::log::start();
+    app.at("/").get(|_| async { Ok("Hello, world!") });
     app.listen("0.0.0.0:3001").await?;
-
     Ok(())
 }
--- api-server/src/routes/users.rs
+++ api-server/src/routes/users.rs
@@ -99,7 +99,7 @@ pub async fn new(mut req: Request<State>) -> tide::Result {
     let document = mongodb::bson::ser::to_document(&user).unwrap();
     let id = users.insert_one(document, None).await?.inserted_id;
 
-    Ok(response_from_json(doc! {"token": id.to_string()}))
+    Ok(response_from_json(doc! {"token": id}))
 }
 
 /// Pass a JSON object with the ObjectId for the user
