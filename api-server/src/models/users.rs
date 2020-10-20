d0038ec1664a1be834baef448fa307396c4abab8
Alexander Jackson <alexanderjackson@protonmail.com>
2020-10-20 14:07:37 +0100

    Revert "Remove the Model trait (#36)"
    This reverts commit 3a8e99566b5ae8e6e503318f76364469846dad8e.

--- api-server/src/models/users.rs
+++ api-server/src/models/users.rs
@@ -1,6 +1,8 @@
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
+use crate::models::model;
+
 // Define a model. Simple as deriving a few traits.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
@@ -8,4 +10,16 @@ pub struct User {
     pub id: Option<ObjectId>,
     pub email: String,
     pub password: String,
+}
+
+impl model::Model for User {
+    const COLLECTION_NAME: &'static str = "users";
+
+    fn id(&self) -> Option<ObjectId> {
+        self.id.clone()
+    }
+
+    fn set_id(&mut self, id: ObjectId) {
+        self.id = Some(id);
+    }
 }
--- api-server/src/routes/users.rs
+++ api-server/src/routes/users.rs
@@ -1,9 +1,12 @@
+use std::str;
+
 use ammonia::clean_text;
 use async_std::stream::StreamExt;
 use mongodb::bson::{doc, document::Document, oid::ObjectId};
 use tide::http::mime;
 use tide::{Request, Response};
 
+use crate::models::model::Model;
 use crate::models::users::User;
 use crate::State;
 
@@ -12,20 +15,14 @@ const PBKDF2_ROUNDS: u32 = 100_000;
 /// This route will take in a user ID in the request and
 /// will return the information for that user
 pub async fn get(req: Request<State>) -> tide::Result {
-    let database = req.state().client.database("sybl");
-    let users = database.collection("users");
-
-    let id: String = req.param("user_id")?;
+    let state = &req.state();
+    let db = &state.client.database("sybl");
+    let id = req.param::<String>("user_id")?;
     let object_id = ObjectId::with_string(&id).unwrap();
-
     let filter = doc! { "_id": object_id };
-    // TODO: This can fail if the user does not exist
-    let document = users.find_one(filter, None).await?.unwrap();
-
-    let json: User = mongodb::bson::de::from_document(document).unwrap();
-
+    let doc = User::find_one(db.clone(), filter, None).await?;
     let response = Response::builder(200)
-        .body(json!(json))
+        .body(json!(doc))
         .content_type(mime::JSON)
         .build();
 
@@ -36,17 +33,17 @@ pub async fn get(req: Request<State>) -> tide::Result {
 /// the find. This will return a JSON object containing multiple
 /// users which fulfill the filter.
 pub async fn filter(mut req: Request<State>) -> tide::Result {
-    let database = req.state().client.database("sybl");
-    let users = database.collection("users");
+    let state = &req.state();
+    let db = &state.client.database("sybl");
     let filter: Document = req.body_json().await?;
-
     println!("Filter: {:?}", &filter);
-
-    let cursor = users.find(filter, None).await?;
-    let documents: Result<Vec<Document>, mongodb::error::Error> = cursor.collect().await;
-
+    let mut cursor = User::find(db.clone(), filter, None).await?;
+    let mut docs: Vec<User> = Vec::new();
+    while let Some(user) = cursor.next().await {
+        docs.push(user?);
+    }
     Ok(Response::builder(200)
-        .body(json!(documents.unwrap()))
+        .body(json!(docs))
         .content_type(mime::JSON)
         .build())
 }
@@ -62,52 +59,42 @@ pub async fn filter(mut req: Request<State>) -> tide::Result {
 /// This will return the user token
 pub async fn new(mut req: Request<State>) -> tide::Result {
     let doc: Document = req.body_json().await?;
-    log::debug!("Document received: {:?}", &doc);
-
-    let state = req.state();
+    let state = &req.state();
+    let db = &state.client.database("sybl");
     let pepper = &state.pepper;
+    let password: &str = &clean_text(doc.get_str("password").unwrap());
+    let email: &str = &clean_text(doc.get_str("email").unwrap());
+    println!("Email: {}, Password: {}", email, password);
 
-    let database = state.client.database("sybl");
-    let users = database.collection("users");
-
-    let password = doc.get_str("password").unwrap();
-    let email = clean_text(doc.get_str("email").unwrap());
-
-    log::info!("Email: {}, Password: {}", email, password);
-
-    let filter = doc! { "email": &email };
-
-    log::info!("Checking if the user exists already");
-
-    if users.find_one(filter, None).await?.is_some() {
-        log::error!("Found a user with email '{}' already", &email);
-        return Ok(Response::builder(200)
-            .body(json!(doc! {"token": "null"}))
-            .content_type(mime::JSON)
-            .build());
-    }
-
-    log::info!("User does not exist, registering them now");
+    let filter = doc! {"email": email};
+    match User::find_one(db.clone(), filter, None).await? {
+        Some(_) => {
+            return Ok(Response::builder(200)
+                .body(json!(doc! {"token": "null"}))
+                .content_type(mime::JSON)
+                .build());
+        }
+        _ => (),
+    };
 
     let peppered = format!("{}{}", &password, &pepper);
 
     let pbkdf2_hash = pbkdf2::pbkdf2_simple(&peppered, PBKDF2_ROUNDS).unwrap();
     let verified = pbkdf2::pbkdf2_check(&peppered, &pbkdf2_hash).is_ok();
 
-    log::info!("Verified: {}", verified);
-    log::info!("Hash: {:?}", pbkdf2_hash);
+    println!("Verified: {}", verified);
+    println!("Hash: {:?}", pbkdf2_hash);
 
-    let user = User {
+    let mut user: User = User {
         id: Some(ObjectId::new()),
-        email: email,
+        email: String::from(email),
         password: pbkdf2_hash,
     };
 
-    let document = mongodb::bson::ser::to_document(&user).unwrap();
-    let id = users.insert_one(document, None).await?.inserted_id;
-
+    user.save(db.clone(), None).await?;
+    let user_id = user.id().unwrap();
     Ok(Response::builder(200)
-        .body(json!(doc! {"token": id}))
+        .body(json!(doc! {"token": user_id.to_string()}))
         .content_type(mime::JSON)
         .build())
 }
@@ -116,42 +103,37 @@ pub async fn new(mut req: Request<State>) -> tide::Result {
 /// which is being edited and the attributes which are being
 /// changed. This should look like:
 /// {
-///     "id": "TOKEN"
+///     "_id": "TOKEN"
 ///     "email": "email@email.com",
 ///     "password": "password"
 /// }
 ///
 /// This will return the status of the transaction
 pub async fn edit(mut req: Request<State>) -> tide::Result {
-    let state = req.state();
-    let database = state.client.database("sybl");
-    let users = database.collection("users");
-
+    let state = &req.state();
+    let db = &state.client.database("sybl");
     let doc: Document = req.body_json().await?;
-    let object_id = clean_text(doc.get_str("id").unwrap());
-    let id = ObjectId::with_string(&object_id).unwrap();
-
+    let id = ObjectId::with_string(&clean_text(doc.get_str("id").unwrap())).unwrap();
     let filter = doc! {"_id": id};
-    let mut user = match users.find_one(filter.clone(), None).await? {
-        Some(u) => mongodb::bson::de::from_document::<User>(u).unwrap(),
-        None => {
+    let mut user = match User::find_one(db.clone(), filter, None).await {
+        Ok(u) => u,
+        Err(_) => {
             return Ok(Response::builder(200)
                 .body(json!(doc! {"status": "failed"}))
                 .content_type(mime::JSON)
                 .build())
         }
-    };
+    }
+    .unwrap();
 
     for key in doc.keys() {
         println!("{}", key);
-
         if key == "email" {
-            user.email = clean_text(doc.get_str(key).unwrap());
+            user.email = String::from(&clean_text(doc.get_str(key).unwrap()));
         }
     }
 
-    let document = mongodb::bson::ser::to_document(&user).unwrap();
-    users.update_one(filter.clone(), document, None).await?;
+    user.save(db.clone(), None).await?;
 
     Ok(Response::builder(200)
         .body(json!(doc! {"status": "changed"}))
@@ -171,24 +153,14 @@ pub async fn edit(mut req: Request<State>) -> tide::Result {
 /// This will return the user token
 pub async fn login(mut req: Request<State>) -> tide::Result {
     let doc: Document = req.body_json().await?;
-
-    let state = req.state();
-    let database = state.client.database("sybl");
+    let state = &req.state();
+    let db = &state.client.database("sybl");
     let pepper = &state.pepper;
-
-    let users = database.collection("users");
-
-    let password = doc.get_str("password").unwrap();
-    let email = clean_text(doc.get_str("email").unwrap());
-
+    let password: &str = &clean_text(doc.get_str("password").unwrap());
+    let email: &str = &clean_text(doc.get_str("email").unwrap());
     println!("{}, {}", &email, &password);
-
     let filter = doc! {"email": email};
-    let user = users
-        .find_one(filter, None)
-        .await?
-        .map(|doc| mongodb::bson::de::from_document::<User>(doc).unwrap());
-
+    let user = User::find_one(db.clone(), filter, None).await?;
     match user {
         Some(user) => {
             let peppered = format!("{}{}", password, pepper);
@@ -197,7 +169,7 @@ pub async fn login(mut req: Request<State>) -> tide::Result {
             if verified {
                 println!("Logged in: {:?}", user);
                 Ok(Response::builder(200)
-                    .body(json!(doc! {"token": user.id.unwrap().to_string()}))
+                    .body(json!(doc! {"token": user.id().unwrap().to_string()}))
                     .content_type(mime::JSON)
                     .build())
             } else {
@@ -221,17 +193,14 @@ pub async fn login(mut req: Request<State>) -> tide::Result {
 /// Delete method. Pass ID as part of JSON object and the corressponding user
 /// will be deleted from the Database.
 pub async fn delete(mut req: Request<State>) -> tide::Result {
-    let doc: Document = req.body_json().await?;
-
     let state = &req.state();
-    let database = &state.client.database("sybl");
-    let users = database.collection("users");
-
-    let object_id = clean_text(doc.get_str("id").unwrap());
-    let id = ObjectId::with_string(&object_id).unwrap();
+    let db = &state.client.database("sybl");
+    let doc: Document = req.body_json().await?;
+    let id = ObjectId::with_string(&clean_text(doc.get_str("id").unwrap())).unwrap();
     let filter = doc! {"_id": id};
-
-    users.find_one_and_delete(filter, None).await.unwrap();
+    User::find_one_and_delete(db.clone(), filter, None)
+        .await
+        .unwrap();
 
     Ok(Response::builder(200)
         .body(json!(doc! {"status": "deleted"}))
