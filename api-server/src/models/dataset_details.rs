bed20f98870d4f1d6b22f4bdb7885af66041c49a
Freddie Brown <fred@noser.net>
2020-10-23 18:09:36 +0100

    created dataset details object

--- api-server/src/models/dataset_details.rs
+++ api-server/src/models/dataset_details.rs
@@ -2,7 +2,6 @@
 
 use mongodb::bson;
 use mongodb::bson::oid::ObjectId;
-use serde::{Deserialize, Serialize};
 
 /// Defines the information that should be stored as details for a project
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,4 +15,5 @@ pub struct DatasetDetails {
     pub date_created: bson::DateTime,
     /// Head of the dataset
     pub head: Option<String>,
+
 }
--- api-server/src/models/mod.rs
+++ api-server/src/models/mod.rs
@@ -1,6 +1,6 @@
 //! Defines the models module for MongoDB
 
-pub mod dataset_details;
 pub mod datasets;
+pub mod dataset_details;
 pub mod projects;
 pub mod users;
