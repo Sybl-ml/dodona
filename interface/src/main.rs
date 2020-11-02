c91ea8300a5a2dec88e0655f61f8326bbbf7e35d
Alexander Jackson <alexanderjackson@protonmail.com>
2020-11-02 18:48:11 +0000

    Add logging and remove connect_timeout
    Add logging to the project using `pretty_env_logger` so that log levels
    can be controlled using `RUST_LOG`. Remove the `connect_timeout` usage
    and replace it with `connect` and `std::thread::sleep` as it didn't seem
    to be working correctly.

--- interface/src/main.rs
+++ interface/src/main.rs
@@ -15,6 +15,8 @@ fn listen(inner: &Arc<Inner>) -> std::io::Result<()> {
         let mut buffer = [0_u8; 24];
         stream.read_exact(&mut buffer)?;
 
+        log::info!("Received: {}", std::str::from_utf8(&buffer).unwrap());
+
         let mut queue = inner.queue.lock().unwrap();
         queue.push_back(buffer);
     }
@@ -25,16 +27,20 @@ fn listen(inner: &Arc<Inner>) -> std::io::Result<()> {
 /// Continually tries to connect until a connection is achieved.
 fn retry_until_connect(address: &SocketAddr, timeout: Duration) -> TcpStream {
     loop {
-        if let Ok(stream) = TcpStream::connect_timeout(address, timeout) {
-            return stream;
+        if let Ok(stream) = TcpStream::connect(address) {
+            break stream;
         }
+
+        log::debug!("Failed to connect to: {}", address);
+        log::debug!("Sleeping for: {:?}", timeout);
+        std::thread::sleep(timeout);
     }
 }
 
 /// Receives messages from the frontend thread and communicates with the DCL.
 fn receive(inner: &Arc<Inner>) -> std::io::Result<()> {
     let address = SocketAddr::from_str("127.0.0.1:6000").unwrap();
-    let timeout = Duration::from_secs(1);
+    let timeout = Duration::from_secs(5);
 
     loop {
         // Lock the queue so it cannot change
@@ -42,6 +48,8 @@ fn receive(inner: &Arc<Inner>) -> std::io::Result<()> {
 
         // Try and send something onwards
         if let Some(element) = queue.pop_front() {
+            log::info!("Sending: {}", std::str::from_utf8(&element).unwrap());
+
             let mut stream = retry_until_connect(&address, timeout);
             stream.write_all(&element)?;
             stream.shutdown(Shutdown::Both)?;
@@ -49,17 +57,23 @@ fn receive(inner: &Arc<Inner>) -> std::io::Result<()> {
     }
 }
 
+/// The expected format for an ObjectId in bytes.
 type ObjectId = [u8; 24];
-type Queue = VecDeque<ObjectId>;
 
+/// Represents the shared structure both threads have access to.
 #[derive(Debug, Default)]
 struct Inner {
-    queue: Mutex<Queue>,
+    /// The internal queue of ObjectIds
+    queue: Mutex<VecDeque<ObjectId>>,
 }
 
 fn main() -> std::io::Result<()> {
+    // Setup logging
+    pretty_env_logger::init();
+
     let inner = Arc::new(Inner::default());
 
+    log::info!("Beginning the thread execution");
     crossbeam::scope(|s| {
         s.spawn(|_| listen(&Arc::clone(&inner)));
         s.spawn(|_| receive(&Arc::clone(&inner)));
