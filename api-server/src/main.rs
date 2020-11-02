c9762a5675b3a16831f634439aff9e226cd642e3
Alexander Jackson <alexanderjackson@protonmail.com>
2020-11-02 12:45:30 +0000

    Log the config values on startup
    Log the configuration that is being resolved alongside the configuration
    values actually being used when the server starts up. This requires the
    Tide logging to be initialised earlier, so that the `log` statements in
    `src/config.rs` actually show up.

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -2,6 +2,8 @@ use dodona::config::Environment;
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
+    tide::log::start();
+
     let environment = if cfg!(debug_assertions) {
         Environment::Development
     } else {
@@ -11,7 +13,6 @@ async fn main() -> Result<(), std::io::Error> {
     dodona::load_config(environment);
     let app = dodona::build_server().await;
 
-    tide::log::start();
     app.listen("0.0.0.0:3001").await?;
 
     Ok(())
