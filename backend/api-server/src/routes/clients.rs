68303f336eddbb98a10ce82f6dd4f4736bf5084f
Graham Kemp <grahamk206@gmail.com>
2020-11-20 18:26:46 +0000

    Added clients model which is updated on registration

--- backend/api-server/src/routes/clients.rs
+++ backend/api-server/src/routes/clients.rs
@@ -2,9 +2,11 @@
 
 use ammonia::clean_text;
 use mongodb::bson::{doc, document::Document, oid::ObjectId};
-use tide::Request;
+use tide::{Request, Response};
 
 use crate::routes::response_from_json;
+use crypto::encoded_key_pair;
+use models::clients::Client;
 use models::users::User;
 
 use crate::State;
@@ -18,15 +20,15 @@ pub async fn register(mut req: Request<State>) -> tide::Result {
     let state = req.state();
     let database = state.client.database("sybl");
     let pepper = &state.pepper;
-
     let users = database.collection("users");
+    let clients = database.collection("clients");
 
     let password = doc.get_str("password").unwrap();
     let email = clean_text(doc.get_str("email").unwrap());
     let id = doc.get_str("id").unwrap();
     let object_id = ObjectId::with_string(&id).unwrap();
 
-    let filter = doc! { "_id": object_id };
+    let filter = doc! { "_id": &object_id };
     let user = users
         .find_one(filter, None)
         .await?
@@ -36,15 +38,38 @@ pub async fn register(mut req: Request<State>) -> tide::Result {
         let peppered = format!("{}{}", password, pepper);
         let verified = pbkdf2::pbkdf2_check(&peppered, &user.password).is_ok();
 
+        // Entered and stored email and password match
         if verified && email == user.email {
-            println!("Logged in: {:?}", user);
-            Ok(response_from_json(doc! {"privKey": 1}))
+            // generate public and private key pair
+            let (public_key, private_key) = encoded_key_pair();
+            // create a new client object
+            users
+                .update_one(
+                    doc! { "_id": &object_id },
+                    doc! {"$set": {"client": true}},
+                    None,
+                )
+                .await?;
+
+            // update user as client
+            let client = Client {
+                id: Some(ObjectId::new()),
+                user_id: Some(object_id),
+                public_key,
+            };
+            // store client object in db
+            let document = mongodb::bson::ser::to_document(&client).unwrap();
+            clients.insert_one(document, None).await?;
+
+            // reponse with private key
+            Ok(response_from_json(doc! {"privKey": private_key}))
         } else {
-            println!("Failed login: wrong password");
-            Ok(response_from_json(doc! {"token": "null"}))
+            Ok(Response::builder(403)
+                .body("email or password incorrect")
+                .build())
         }
     } else {
-        println!("Failed login: wrong email");
+        println!("User ID does not exist");
         Ok(response_from_json(doc! {"token": "null"}))
     }
 }
