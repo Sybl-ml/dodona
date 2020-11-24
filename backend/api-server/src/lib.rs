421f01681ee1a4911071fb76ab3a8c70e92ff4e9
Nathan Hall <hallnath42@gmail.com>
2020-11-24 15:20:52 +0000

    New model route

--- backend/api-server/src/lib.rs
+++ backend/api-server/src/lib.rs
@@ -108,6 +108,9 @@ pub async fn build_server() -> tide::Server<State> {
 
     let mut client_api = core_api.at("/clients");
     client_api.at("/register").post(routes::clients::register);
+    client_api
+        .at("/u/:user_id")
+        .get(routes::clients::get_user_models);
     client_api.at("/m/new").post(routes::clients::new_model);
 
     // CORS
