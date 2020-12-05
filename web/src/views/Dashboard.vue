11af99b52687bc34f05b3ecdd4e66fe5aeb527a4
Nathan Hall <hallnath42@gmail.com>
2020-12-05 02:56:17 +0000

    New projects update when submitted

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -68,6 +68,7 @@
             @update:description="updateDescription"
             @update:name="updateName"
             @delete:project="deleteProject"
+            @update:add="addProject"
           ></router-view>
         </b-col>
       </b-row>
@@ -143,6 +144,22 @@ export default {
           break;
         }
       }
+    },
+    async addProject(id) {
+      let project_response = await axios.get(
+        `http://localhost:3001/api/projects/p/${id}`
+      );
+
+      let x = project_response.data.project;
+      let y = {
+        ...x,
+        id: x._id.$oid,
+        user_id: x.user_id.$oid,
+        date_created: x.date_created.$date,
+      };
+      delete y._id;
+
+      this.projects.push(y)
     },
     deleteProject(id) {
       let index = 0;
