9e75949bc78e88490656aa4252c73878a128329d
Alexander Jackson <alexanderjackson@protonmail.com>
2021-01-07 18:07:52 +0000

    Update interface to use Tokio 1.0
    Update the interface to be asynchronous, using Tokio 1.0 for its runtime
    inline with most other components.

--- backend/interface/src/main.rs
+++ backend/interface/src/main.rs
@@ -1,22 +1,24 @@
 use std::collections::VecDeque;
 use std::env;
-use std::io::{Read, Write};
-use std::net::{Ipv4Addr, Shutdown, SocketAddrV4, TcpListener, TcpStream};
+use std::net::{Ipv4Addr, SocketAddrV4};
 use std::str::FromStr;
 use std::sync::{Arc, Condvar, Mutex};
 use std::time::Duration;
 
-use async_std::stream::StreamExt;
-use async_std::task::block_on;
+use anyhow::Result;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_stream::StreamExt;
 
 use config::Environment;
+use messages::InterfaceMessage;
 use models::jobs::Job;
 use utils::setup_logger;
 
 const TIMEOUT_SECS: u64 = 1;
 
 /// Listens for incoming messages from the API server and forwards them to the queue.
-fn listen(inner: &Arc<Inner>) -> std::io::Result<()> {
+async fn listen(inner: &Arc<Inner>) -> Result<()> {
     // Get the environment variable for listening
     let var = env::var("INTERFACE_LISTEN").expect("INTERFACE_LISTEN must be set");
     let port = u16::from_str(&var).expect("INTERFACE_LISTEN must be a u16");
@@ -25,35 +27,37 @@ fn listen(inner: &Arc<Inner>) -> std::io::Result<()> {
     let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, port);
 
     // Begin listening for messages
-    let listener = TcpListener::bind(addr)?;
-    let incoming = listener.incoming();
-
+    let listener = TcpListener::bind(addr).await?;
     log::info!("Listening for connections on: {}", addr);
 
-    for possible_stream in incoming {
-        let mut stream = possible_stream?;
+    loop {
+        let (mut stream, address) = listener.accept().await?;
+        log::debug!("Received connection from: {}", address);
+
         let mut buffer = [0_u8; 24];
-        stream.read_exact(&mut buffer)?;
+        let message = InterfaceMessage::from_stream(&mut stream, &mut buffer).await?;
 
         log::info!("Received: {}", std::str::from_utf8(&buffer).unwrap());
 
         let mut queue = inner.queue.lock().unwrap();
-        queue.push_back(buffer);
+        queue.push_back(message);
 
         // Alert the other thread
         drop(queue);
         inner.available.notify_one();
     }
-
-    Ok(())
 }
 
 /// Continually tries to connect until a connection is achieved.
-fn try_to_connect(address: &SocketAddrV4, timeout: Duration, attempts: usize) -> Option<TcpStream> {
+async fn try_to_connect(
+    address: &SocketAddrV4,
+    timeout: Duration,
+    attempts: usize,
+) -> Option<TcpStream> {
     for i in 0..attempts {
         log::debug!("Connection attempt: {}", i + 1);
 
-        if let Ok(stream) = TcpStream::connect(address) {
+        if let Ok(stream) = TcpStream::connect(address).await {
             return Some(stream);
         }
 
@@ -68,7 +72,7 @@ fn try_to_connect(address: &SocketAddrV4, timeout: Duration, attempts: usize) ->
 }
 
 /// Receives messages from the frontend thread and communicates with the DCL.
-fn receive(inner: &Arc<Inner>) -> std::io::Result<()> {
+async fn receive(inner: &Arc<Inner>) -> Result<()> {
     // Get the environment variable for sending
     let var = env::var("INTERFACE_SOCKET").expect("INTERFACE_SOCKET must be set");
     let port = u16::from_str(&var).expect("INTERFACE_SOCKET must be a u16");
@@ -84,11 +88,11 @@ fn receive(inner: &Arc<Inner>) -> std::io::Result<()> {
     loop {
         // Try and send something onwards
         if let Some(element) = queue.pop_front() {
-            if let Some(mut stream) = try_to_connect(&addr, timeout, attempts) {
+            if let Some(mut stream) = try_to_connect(&addr, timeout, attempts).await {
                 // Send the element to the onward node that we connected to
-                stream.write_all(&element)?;
-                log::info!("Sent: {}", std::str::from_utf8(&element).unwrap());
-                stream.shutdown(Shutdown::Both)?;
+                stream.write_all(&element.as_bytes()).await?;
+                log::info!("Sent: {:?}", element);
+                stream.shutdown().await?;
             } else {
                 // Readd the element back to the queue at the front
                 queue.push_front(element);
@@ -104,20 +108,17 @@ fn receive(inner: &Arc<Inner>) -> std::io::Result<()> {
     }
 }
 
-/// The expected format for an ObjectId in bytes.
-type ObjectId = [u8; 24];
-
 /// Represents the shared structure both threads have access to.
 #[derive(Debug, Default)]
 struct Inner {
-    /// The internal queue of ObjectIds
-    queue: Mutex<VecDeque<ObjectId>>,
+    /// The internal queue of jobs
+    queue: Mutex<VecDeque<InterfaceMessage>>,
     /// The semaphore for alerting threads
     available: Condvar,
 }
 
 impl Inner {
-    pub fn with_state(initial: VecDeque<ObjectId>) -> Self {
+    pub fn with_state(initial: VecDeque<InterfaceMessage>) -> Self {
         log::info!("Initialising the queue with {} elements", initial.len());
 
         Self {
@@ -127,35 +128,31 @@ impl Inner {
     }
 }
 
-fn get_job_queue() -> mongodb::error::Result<VecDeque<ObjectId>> {
+async fn get_job_queue() -> mongodb::error::Result<VecDeque<InterfaceMessage>> {
     // Setup the MongoDB client
     let uri = std::env::var("CONN_STR").unwrap();
-    let client = block_on(async { mongodb::Client::with_uri_str(&uri).await })?;
+    let client = mongodb::Client::with_uri_str(&uri).await?;
 
     // Get the jobs collection
     let database = client.database("sybl");
     let jobs = database.collection("jobs");
 
     // Pull all the jobs and deserialize them
-    let cursor = block_on(async { jobs.find(None, None).await })?;
+    let cursor = jobs.find(None, None).await?;
 
-    let queue = block_on(async {
-        cursor
-            .filter_map(Result::ok)
-            .filter_map(|x| mongodb::bson::de::from_document::<Job>(x).ok())
-            .map(|job| {
-                let mut bytes = [0_u8; 24];
-                bytes.copy_from_slice(&job.msg.as_bytes()[..]);
-                bytes
-            })
-            .collect()
-            .await
-    });
+    // TODO: Change this to collect once by iterating over the cursor
+    let queue: Vec<InterfaceMessage> = cursor
+        .filter_map(Result::ok)
+        .filter_map(|x| mongodb::bson::de::from_document::<Job>(x).ok())
+        .map(|x| x.msg)
+        .collect()
+        .await;
 
-    Ok(queue)
+    Ok(queue.into_iter().collect())
 }
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     // Setup logging
     setup_logger("interface");
 
@@ -168,15 +165,15 @@ fn main() -> std::io::Result<()> {
 
     config::load(environment);
 
-    let state = get_job_queue().expect("Failed to get the job queue");
+    let state = get_job_queue().await.expect("Failed to get the job queue");
     let inner = Arc::new(Inner::with_state(state));
 
     log::info!("Beginning the thread execution");
-    crossbeam::scope(|s| {
-        s.spawn(|_| listen(&Arc::clone(&inner)));
-        s.spawn(|_| receive(&Arc::clone(&inner)));
-    })
-    .unwrap();
+
+    let left = Arc::clone(&inner);
+    let right = Arc::clone(&inner);
+
+    tokio::try_join!(listen(&left), receive(&right))?;
 
     Ok(())
 }
