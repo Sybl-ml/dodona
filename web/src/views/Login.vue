aaa1bfa26947ddf116e86220d48120f7f6cc6af8
Nathan Hall <hallnath42@gmail.com>
2021-01-26 18:53:52 +0000

    Login layout changes

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,57 +1,61 @@
 <template>
   <b-container fluid>
     <b-row class="justify-content-center text-center">
-      <b-col lg="3" md="6" sm="8" xs="12">
-        <br /><br />
-        <icon-logo height="5em" width="5em" :show_text="false" />
-        <h1>Sign In To Sybl</h1>
-        <b-form class="mt-5 mb-3" @submit.prevent="onSubmit">
-          <b-form-input
-            class="mb-3"
-            type="email"
-            required
-            placeholder="Enter Email"
-            v-model="email"
-          />
+      <b-col lg="4" md="6" sm="8" xs="12">
+        <icon-logo
+          class="mt-5 mb-3"
+          height="10em"
+          width="10em"
+          :show_text="false"
+        />
+        <h1 class="mb-3"><strong>Please Sign in</strong></h1>
+        <b-card bordered-variant="primary" class="text-center mt-3 mb-5">
+          <b-form class="mt-3 mb-3" @submit.prevent="onSubmit">
+            <b-input-group class="mb-3" prepend="@">
+              <b-form-input
+                type="email"
+                required
+                placeholder="Enter Email"
+                v-model="email"
+              />
+            </b-input-group>
 
-          <b-input-group class="mb-3">
-            <b-form-input
-              :type="passwordType"
-              required
-              placeholder="Password"
-              v-model="password"
-              pattern="^.{8,32}$"
-              title="Password must contain at least eight characters"
-            />
-            <template #append>
-              <b-input-group-text>
-                <b-icon
-                  style="cursor: pointer;"
-                  :icon="passwordIcon"
-                  @click="hidePassword = !hidePassword"
-                />
-              </b-input-group-text>
-            </template>
-          </b-input-group>
+            <b-input-group class="mb-3" prepend="#">
+              <b-form-input
+                :type="passwordType"
+                required
+                placeholder="Password"
+                v-model="password"
+                pattern="^.{8,32}$"
+                title="Password must contain at least eight characters"
+              />
+              <template #append>
+                <b-input-group-text>
+                  <b-icon
+                    style="cursor: pointer;"
+                    :icon="passwordIcon"
+                    @click="hidePassword = !hidePassword"
+                  />
+                </b-input-group-text>
+              </template>
+            </b-input-group>
 
-          <b-form-checkbox
-            style="display:none;"
-            value="me"
-            class="mb-3"
-            v-model="remember_password"
-            >Remember Me</b-form-checkbox
-          >
-          <b-button variant="primary" type="submit" block>
-            SIGN IN
-            <b-spinner v-show="submitted" small></b-spinner>
-          </b-button>
-        </b-form>
-
-        <a href="/forgot">Forgotten Password?</a>
-
-        <br />
-        <br />
+            <b-form-checkbox
+              style="display:none;"
+              value="me"
+              class="mb-3"
+              v-model="remember_password"
+              >Remember Me</b-form-checkbox
+            >
+            <b-button class="mb-3" variant="primary" type="submit" block>
+              SIGN IN
+              <b-spinner v-show="submitted" small></b-spinner>
+            </b-button>
+            <a href="/forgot">Forgotten Password?</a>
+          </b-form>
+        </b-card>
       </b-col>
+      
     </b-row>
     <b-row class="justify-content-center text-center">
       <b-alert v-model="failed" variant="danger" dismissible>
@@ -63,6 +67,7 @@
 
 <script>
 import IconLogo from "@/components/icons/IconLogo";
+import Face from "@/components/icons/conent/Face";
 import axios from "axios";
 export default {
   data() {
@@ -77,6 +82,7 @@ export default {
   },
   components: {
     IconLogo,
+    Face,
   },
   computed: {
     passwordType() {
