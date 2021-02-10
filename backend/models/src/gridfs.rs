cdf5eee8007b46e2b3d2a19959d0e26e2d32d332
Alexander Jackson <alexanderjackson@protonmail.com>
2021-02-10 12:25:14 +0000

    Begin adding large file support
    Currently, users are constrained to approximately 32Kb of data in their
    uploaded datasets. Realistically, many users are going to want to
    process huge amounts more, so we need to allow larger upload sizes.
    
    The easiest way to do this is through the multipart form specification.
    This allows unlimited upload sizes, as long as the web server can
    continue to handle it. However, we then need to actually store the files
    in a viable way for future processing.
    
    MongoDB advises against having large documents in the database, with the
    theoretical boundary being 16MB. While MongoDB has large file support in
    the form of GridFS, the current driver does not support it. Instead, we
    can make our own GridFS-like models, which contain a `File` and a
    `Chunk` for streaming data. This allows us to store large files
    efficiently and also process them through streaming later.
    
    Currently, this is very experimental. The API server will happily upload
    the data, but no additional processing is done and they are not linked
    well to the DCL and other components. Instead, a blank dataset will be
    stored where other components expect to find it.

---
+++ backend/models/src/gridfs.rs
@@ -0,0 +1,114 @@
+use bytes::Bytes;
+use chrono::Utc;
+use mongodb::bson::{self, document::Document, oid::ObjectId};
+
+/// Represents the metadata of a file in the database.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct File {
+    /// The unique identifier for this file
+    #[serde(rename = "_id")]
+    pub id: ObjectId,
+    /// The length of the file, measured in bytes
+    pub length: i32,
+    /// The number of chunks this file is represented as
+    // TODO: MongoDB uses a `chunk_size` parameter instead
+    pub chunk_count: i32,
+    /// The date and time the file was uploaded
+    pub upload_date: bson::DateTime,
+    /// The name of the file
+    pub filename: String,
+    /// Any user defined metadata to go with the file
+    pub metadata: Document,
+}
+
+impl File {
+    /// Creates a new file given the filename.
+    ///
+    /// Note, this only creates the instance locally, it does not store any data in the database
+    /// yet.
+    pub fn new(filename: String) -> Self {
+        Self {
+            id: ObjectId::new(),
+            length: 0,
+            chunk_count: 0,
+            upload_date: bson::DateTime(Utc::now()),
+            filename,
+            metadata: Document::new(),
+        }
+    }
+
+    /// Uploads a new [`Chunk`] to the database and updates the internal state of the [`File`].
+    pub async fn upload_chunk(
+        &mut self,
+        database: &mongodb::Database,
+        bytes: &Bytes,
+    ) -> mongodb::error::Result<()> {
+        // Create a new chunk for the given data
+        let chunk = Chunk::new(self.id.clone(), self.chunk_count, bytes.to_vec());
+
+        // Update the length and chunk count
+        self.length += bytes.len() as i32;
+        self.chunk_count += 1;
+
+        // Upload the chunk itself
+        chunk.upload(&database).await?;
+
+        Ok(())
+    }
+
+    /// Indicates there is no more data and uploads the [`File`] to the database.
+    pub async fn finalise(&self, database: &mongodb::Database) -> mongodb::error::Result<()> {
+        // Get the files collection
+        let files = database.collection("files");
+
+        // Convert the file to a document
+        let document = bson::ser::to_document(&self).unwrap();
+
+        // Upload it to the relevant
+        files.insert_one(document, None).await?;
+
+        Ok(())
+    }
+}
+
+/// Represents a chunk of a file in the database.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Chunk {
+    /// The unique identifier for this chunk
+    #[serde(rename = "_id")]
+    pub id: ObjectId,
+    /// The identifier of the file it belongs to
+    pub files_id: ObjectId,
+    /// The index number of this chunk
+    pub n: i32,
+    /// The data stored within the chunk
+    pub data: bson::Binary,
+}
+
+impl Chunk {
+    /// Creates a new [`Chunk`] referring to a [`File`].
+    pub fn new(files_id: ObjectId, n: i32, data: Vec<u8>) -> Self {
+        Self {
+            id: ObjectId::new(),
+            files_id,
+            n,
+            data: bson::Binary {
+                subtype: bson::spec::BinarySubtype::Generic,
+                bytes: data,
+            },
+        }
+    }
+
+    /// Uploads the chunk into the database.
+    pub async fn upload(&self, database: &mongodb::Database) -> mongodb::error::Result<()> {
+        let chunks = database.collection("chunks");
+
+        // Convert the struct to a document
+        let document = bson::ser::to_document(&self).unwrap();
+
+        // Upload the document
+        chunks.insert_one(document, None).await?;
+
+        Ok(())
+    }
+}
