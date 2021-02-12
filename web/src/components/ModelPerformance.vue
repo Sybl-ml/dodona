7bdbe0d21f72545cc3c7337e3b01b40cb6e48983
Graham Kemp <grahamk206@gmail.com>
2021-02-12 18:22:02 +0000

    Will load the graph is not previously loaded
    Adds a chartLoaded bool to data which is initialized as false
    When this component receives the event from the parent component
    checks if chart has been loaded, if not calls renderChartOnCollapse

--- web/src/components/ModelPerformance.vue
+++ web/src/components/ModelPerformance.vue
@@ -1,65 +1,57 @@
 <template>
-<b-container fluid>
+  <b-container fluid>
     <b-row class="justify-content-center">
-        Model Performance: {{this.data}}
-        <bar-chart :chartdata="chartdata" :options="options"/>
-
+      Model Performance: {{ this.data }}
+      <bar-chart :chartdata="chartdata" :options="options" ref="chart" />
     </b-row>
-</b-container>
+  </b-container>
 </template>
 
 <style>
 .nodeExpansion {
-    width: 100%;
+  width: 100%;
 }
 </style>
 
 <script>
-import BarChart from '@/components/charts/BarChart.vue'
+import BarChart from "@/components/charts/BarChart.vue";
 
 export default {
-    name: "ModelPerformance",
-    props: {
-        data: Array,
+  name: "ModelPerformance",
+  props: {
+    data: Array,
+  },
+  data() {
+    return {
+      performance: [],
+      password: "",
+      chartdata: {
+        labels: [1, 2, 3, 4, 5],
+        datasets: [
+          {
+            label: "Performance",
+            backgroundColor: "#f87979",
+            data: [0.5, 0.3, 0.1, 0],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+      chartLoaded: false,
+    };
+  },
+  components: {
+    BarChart,
+  },
+  methods: {
+    show() {
+      if (!this.chartLoaded) {
+        this.$refs.chart.renderChartOnCollapse();
+        this.chartLoaded = true;
+      }
     },
-    data() {
-        return {
-            performance: [],
-            password: "",
-            chartdata: {
-                labels: [1, 2, 3, 4, 5],
-                datasets: [{
-                            label: 'Performance',
-                            backgroundColor: '#f87979',
-                            data: [0.5, 0.4, 0.1, 0]
-                        }
-                    ],
-
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false
-            }
-        }
-    },
-    components: {
-      BarChart
-    },
-    mounted() {
-
-        this.chartdata = {
-            labels: [1, 2, 3, 4, 5],
-            datasets:[{
-                            label: 'Performance',
-                            backgroundColor: '#f87979',
-                            data: this.data
-
-                        }
-                    ],
-
-        }
-
-    }
-
+  },
 };
 </script>
