c72be47c166d5b5a2cc85a18c99805a3bee2e7ef
Alexander Jackson <alexanderjackson@protonmail.com>
2021-02-16 10:03:01 +0000

    Add a failing test for #269
    Add a test that fails on purpose that should test #269. This prevents
    this branch from being merged without fixing the issue at some point.

--- backend/dcl/src/protocol/tests.rs
+++ backend/dcl/src/protocol/tests.rs
@@ -129,3 +129,52 @@ async fn incorrect_ordering_fails() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn protocol_cares_about_api_responses() -> Result<(), Box<dyn Error>> {
+    // Setup the API server mocking
+    let authenticate = mock(
+        "POST",
+        "/api/clients/m/5fe8b9d85511355cdab720aa/authenticate",
+    )
+    .with_status(401)
+    .create();
+
+    // Bind to a random unused TCP port
+    let socket = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0);
+    let listener = TcpListener::bind(socket).await?;
+    let addr = listener.local_addr()?;
+
+    let handler = tokio::spawn(async move {
+        // Accept a single stream
+        let mut stream = listener.accept().await.unwrap().0;
+
+        // Setup the handler and get the access token
+        let mut handler = protocol::Handler::new(&mut stream);
+        let token = handler.get_access_token().await.unwrap().unwrap().1;
+
+        assert_eq!(token, "abc");
+    });
+
+    // Wait for the handler to be ready
+    tokio::time::sleep(Duration::from_millis(100)).await;
+
+    // Connect to the handler
+    let mut stream = TcpStream::connect(addr).await?;
+    let message = ClientMessage::AccessToken {
+        id: String::from("5fe8b9d85511355cdab720aa"),
+        token: String::from("abc"),
+    };
+
+    // Write our access token and shutdown the stream
+    stream.write(&message.as_bytes()).await?;
+    stream.shutdown().await?;
+
+    // Ensure the listener handled it correctly
+    assert!(handler.await.is_err());
+
+    // Ensure the mock handler got called
+    authenticate.assert();
+
+    Ok(())
+}
