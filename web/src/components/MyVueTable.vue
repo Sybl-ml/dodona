adfd56e461f35d3ef55e00aaf175e4d17401a28c
Freddie Brown <fred@noser.net>
2021-02-19 15:36:20 +0000

    got pagination working but CSS on bar below not showing

--- web/src/components/MyVueTable.vue
+++ web/src/components/MyVueTable.vue
@@ -1,23 +1,16 @@
 <template>
-  <div>
-    {{display_data}}
+  <div class="ui container">
     <vuetable ref="vuetable"
-      :api-mode="false"
-      :data="display_data"
-      :fields="fields"
-      pagination-path=""
-      :per-page="5"
-      @vuetable:pagination-data="onPaginationData"
+			:api-mode="false"
+			:fields="fields"
+      :per-page="perPage"
+			:data-manager="dataManager"
+      pagination-path="pagination"
+	    @vuetable:pagination-data="onPaginationData"
     ></vuetable>
-
-    <div class="pagination ui basic segment grid">
-      <vuetable-pagination-info ref="paginationInfo"
-      ></vuetable-pagination-info>
-      
-      <vuetable-pagination ref="pagination"
-        @vuetable-pagination:change-page="onChangePage"
-      ></vuetable-pagination>
-    </div>
+    <vuetable-pagination ref="pagination"
+      @vuetable-pagination:change-page="onChangePage"
+    ></vuetable-pagination>
   </div>
 </template>
 
@@ -25,6 +18,7 @@
 import Vuetable from "vuetable-2/src/components/Vuetable";
 import VuetablePagination from "vuetable-2/src/components/VuetablePagination";
 import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo';
+import _ from "lodash";
 
 export default {
   name: "MyVuetable",
@@ -34,24 +28,46 @@ export default {
     VuetablePaginationInfo
   },
   props: {
-    display_data: String,
+    data: Array,
     fields: Array,
+    perPage: Number
   },
 
   methods: {
     onPaginationData(paginationData) {
       this.$refs.pagination.setPaginationData(paginationData);
-      this.$refs.paginationInfo.setPaginationData(paginationData);
     },
     onChangePage(page) {
       this.$refs.vuetable.changePage(page);
     },
+    dataManager(sortOrder, pagination) {
+      if (this.data.length < 1) return;
+
+      let local = this.data;
+
+      // sortOrder can be empty, so we have to check for that as well
+      if (sortOrder.length > 0) {
+        console.log("orderBy:", sortOrder[0].sortField, sortOrder[0].direction);
+        local = _.orderBy(
+          local,
+          sortOrder[0].sortField,
+          sortOrder[0].direction
+        );
+      }
+
+      pagination = this.$refs.vuetable.makePagination(
+        local.length,
+        this.perPage
+      );
+      console.log('pagination:', pagination)
+      let from = pagination.from - 1;
+      let to = from + this.perPage;
+
+      return {
+        pagination: pagination,
+        data: _.slice(local, from, to)
+      };
+    },
   }
 };
 </script>
-
-<style>
-  .pagination {
-    margin-top: 1rem;
-  }
-</style>
