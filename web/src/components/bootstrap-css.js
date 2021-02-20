a48bae3eddfda94fa0278711fe2e5f206858531e
Nathan Hall <hallnath42@gmail.com>
2021-02-20 22:30:17 +0000

    Changed custom styling to use i for bootstrap icons

--- web/src/components/bootstrap-css.js
+++ web/src/components/bootstrap-css.js
@@ -1,24 +1,24 @@
 export default {
-    tableClass: 'table table-striped',
-    loadingClass: 'loading',
-    ascendingIcon: 'fa fa-chevron-up',
-    descendingIcon: 'fa fa-chevron-down',
-    handleIcon: 'bi bi-menu-hamburger',
-    renderIcon(classes, options) {
-        return `<i class="${classes.join(' ')}"></span>`
-      },
-    pagination: {
-      infoClass: 'pull-left',
-      wrapperClass: 'vuetable-pagination pull-right',
-      activeClass: 'btn-primary',
-      disabledClass: 'disabled',
-      pageClass: 'btn btn-border',
-      linkClass: 'btn btn-border',
-      icons: {
-        first: '',
-        prev: '',
-        next: '',
-        last: '',
-      },
+  tableClass: "table table-striped",
+  loadingClass: "loading",
+  ascendingIcon: "bi bi-caret-up-fill",
+  descendingIcon: "bi bi-caret-down-fill",
+  handleIcon: " ",
+  renderIcon(classes, options) {
+    return `<i class="${classes.join(" ")}"></i>`;
+  },
+  pagination: {
+    infoClass: "pull-left",
+    wrapperClass: "vuetable-pagination pull-right",
+    activeClass: "btn-primary",
+    disabledClass: "disabled",
+    pageClass: "btn btn-border",
+    linkClass: "btn btn-border",
+    icons: {
+      first: "",
+      prev: "",
+      next: "",
+      last: "",
     },
-  }
+  },
+};
