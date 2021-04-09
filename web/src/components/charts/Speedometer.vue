52a5d2889b0365d599f1e297ca7ac93af74ee7b9
hallnath1 <hallnath42@gmail.com>
2021-04-09 16:15:14 +0100

    Created custom speedometer for model performance

---
+++ web/src/components/charts/Speedometer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <vue-speedometer
+      :value="0.4"
+      :needleTransitionDuration="4000"
+      needleTransition="easeElastic"
+      :needleHeightRatio="0.75"
+      :maxSegmentLabels="5"
+      :segments="20"
+      :minValue="0"
+      :maxValue="1"
+      needleColor="#6391ff"
+      :forceRender="true"
+    />
+  </div>
+</template>
+<script>
+import VueSpeedometer from "vue-speedometer";
+
+export default {
+  name: "Speedometer",
+  components: {
+    VueSpeedometer,
+  },
+};
+</script>
