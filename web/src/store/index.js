91786625c61c1e4e2cdffc9e315df8036338a98b
Freddie Brown <fred@noser.net>
2021-04-18 11:48:14 +0100

    solved header dropdown issue

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -85,6 +85,9 @@ export default new Vuex.Store({
     setUser(state, user) {
       Vue.set(state, "user_data", user);
     },
+    updateUser(state) {
+      Vue.set(state.user_data, "client", true);
+    },
     setAvatar(state, avatar) {
       Vue.set(state.user_data, "avatar", avatar);
     },
@@ -313,9 +316,23 @@ export default new Vuex.Store({
         dob: dob,
       });
     },
+
     async generatePrivateKey({ commit }) {
       console.log("Generating new private key");
       return $http.post("api/clients/generatePrivateKey");
+    },
+    async client_register ({commit}, {id, email, password}) {
+      let response = await $http.post(
+        "api/clients/register",
+        {
+          id: id,
+          email: email,
+          password: password,
+        }
+      );
+      commit("updateUser");
+      return response;
+
     },
     async uploadAvatar(context, avatar) {
       return $http.post("api/users/avatar", {
