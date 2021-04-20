fceaff3341e783527dd3e27a92e4c2cd0c338b24
Graham Kemp <grahamk206@gmail.com>
2021-04-20 21:41:48 +0100

    Added papaparse

--- web/src/store/modules/projects.js
+++ web/src/store/modules/projects.js
@@ -1,5 +1,6 @@
 import router from "../../router";
 import $http from "../../services/axios-instance";
+import Papa from "papaparse";
 import Vue from "vue";
 
 function unpackProjectResponse(response) {
