8485bcec4e55a5b65cfd158fa725eefb980cd09c
hallnath1 <hallnath42@gmail.com>
2021-04-20 22:49:21 +0100

    Removed dropdown for add new model for a link

--- web/src/views/Models.vue
+++ web/src/views/Models.vue
@@ -20,6 +20,22 @@
         <b-button
           block
           class="mb-4 shadow"
+          variant="primary"
+          style="border: none"
+          onfocus="this.blur();"
+          target="_blank" 
+          href="https://www.notion.so/Become-a-Client-581ccdc92e2946159ad94e3dec44f72e"
+        >
+          <b-icon-plus-circle-fill></b-icon-plus-circle-fill>
+          Add New Node
+        </b-button>
+      </b-col>
+    </b-row>
+    <!-- <b-row class="justify-content-center">
+      <b-col xs="12" lg="6">
+        <b-button
+          block
+          class="mb-4 shadow"
           v-b-toggle.collapse-new
           variant="primary"
           style="border: none"
@@ -39,79 +55,14 @@
             style="border: none"
             onfocus="this.blur();"
           >
-            <b-card-body title-tag="h5">
-              <b-card-title>Connect a New Node</b-card-title>
-              <p>
-                Execute the below script on your host compute node to get the
-                Sybl-CLI
-              </p>
-              <b-row class="justify-content-center">
-                <b-col xs="12" lg="10">
-                  <b-card class="shadow">
-                    <b-row>
-                      <b-col md="10">
-                        <code>{{ cli_code }}</code>
-                      </b-col>
-                      <b-col style="text-align: right" md="2">
-                        <b-button
-                          no-body
-                          variant="dark"
-                          style="
-                            background: none;
-                            border: none;
-                            margin: 0;
-                            padding: 0;
-                          "
-                          onfocus="this.blur();"
-                        >
-                          <b-icon-clipboard-plus
-                            @click="copy(cli_code)"
-                          ></b-icon-clipboard-plus>
-                        </b-button>
-                      </b-col>
-                    </b-row>
-                  </b-card>
-                </b-col>
-              </b-row>
-              <br />
-              <p>
-                Then run the following commands to connect your compute node the
-                Sybl servers
-              </p>
-              <b-row class="justify-content-center">
-                <b-col xs="12" lg="10">
-                  <b-card class="shadow">
-                    <b-row>
-                      <b-col md="10">
-                        <code>{{ cli_setup }}</code>
-                      </b-col>
-                      <b-col style="text-align: right" md="2">
-                        <b-button
-                          no-body
-                          variant="dark"
-                          style="
-                            background: none;
-                            border: none;
-                            margin: 0;
-                            padding: 0;
-                          "
-                          onfocus="this.blur();"
-                        >
-                          <b-icon-clipboard-plus
-                            @click="copy(cli_setup)"
-                          ></b-icon-clipboard-plus>
-                        </b-button>
-                      </b-col>
-                    </b-row>
-                  </b-card>
-                </b-col>
-              </b-row>
-              <br />
-            </b-card-body>
+            <b-embed
+              type="iframe"
+              src="https://www.notion.so/Become-a-Client-3e39d2a2900a417eae259525216f8aec"
+              ></b-embed>
           </b-card>
         </b-collapse>
       </b-col>
-    </b-row>
+    </b-row> -->
   </b-container>
 </template>
 
