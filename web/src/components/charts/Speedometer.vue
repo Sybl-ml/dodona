4611f4a839f2301d1f2e5322677d24f0040bdc85
Freddie Brown <fred@noser.net>
2021-04-24 10:54:21 +0100

    rounding spedometer number length

---
+++ web/src/components/charts/Speedometer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <vue-speedometer
+      :value="value()"
+      :needleTransitionDuration="4000"
+      needleTransition="easeElastic"
+      :needleHeightRatio="0.75"
+      :maxSegmentLabels="4"
+      :segments="20"
+      :minValue="0"
+      :maxValue="1"
+      needleColor="#6391ff"
+      :forceRender="true"
+      :height="200"
+      :width="250"
+      currentValueText="Model Performance: ${value}"
+    />
+  </div>
+</template>
+<script>
+import VueSpeedometer from "vue-speedometer";
+
+export default {
+  name: "Speedometer",
+  components: {
+    VueSpeedometer,
+  },
+  props: {
+    performance: Array,
+  },
+  methods: {
+    value () {
+      if (this.performance.length == 0) {
+        return 0
+      }
+      else {
+        return Math.round(((this.performance.reduce(function (total, num){return total + num}))/this.performance.length) * 100) / 100;
+      }
+
+    }
+  }
+};
+</script>
